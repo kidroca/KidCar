899f1a926ebe689b2b8fa75e50b531aa56df2fcd
Peter Velkov <kidroca@gmail.com>
2021-03-04 12:40:32 +0200

    Add project files.

---
+++ KidCar/KidCar.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		KidCar.ino
+ Created:	3/4/2021 12:39:38 PM
+ Author:	kidro
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
