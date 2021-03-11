4c80d6d3b3591604d70aff7abbf8d881b7421654
Peter Velkov <kidroca@gmail.com>
2021-03-11 21:21:34 +0200

    Use remote control drving on Arduino Uno

--- KidCar/KidCar.ino
+++ KidCar/KidCar.ino
@@ -4,32 +4,31 @@
  Author:	kidro
 */
 
+#include <IRremote.h>
 #include "LoopHandler.h"
-#include "DrvingWithUltrasoundSensor.h"
 #include "DrivingBehavior.h"
+#include "DrivingWithRemote.h"
 #include "HBridgeDriver.h"
-#include "UltrasoundSensor.h"
 
 #define LED_PIN A0
 #define TRIG_PIN 12
-#define ECHO_PIN 2
+#define RECV_PIN 2
 
-#define ENA 5
-#define IN1 6
-#define IN2 7
-#define IN3 8
-#define IN4 9
-#define ENB 10
+#define ENA 10
+#define IN1 9
+#define IN2 8
+#define IN3 7
+#define IN4 6
+#define ENB 5
 
 HBridgeDriver* driver;
-UltrasoundSensor* sensor;
 DrivingBehavior* drivingBehavior;
 
 void setup() {
 	driver = new HBridgeDriver(ENA, IN1, IN2, IN3, IN4, ENB);
-	sensor = new UltrasoundSensor(TRIG_PIN, ECHO_PIN);
-	drivingBehavior = new DrvingWithUltrasoundSensor(driver, sensor, LED_PIN);
+	drivingBehavior = new DrivingWithRemote(driver, RECV_PIN);
 	drivingBehavior->init();
+
 }
 
 void loop() {
